d2573eb8ac7760e8839966e6bfb2c596e48f6cbc
Melchi031 <vae_soli@live.fr>
2020-02-01 15:32:22 -0500

    Fixed animator controller

--- Assets/Scripts/MyCharacterController.cs
+++ Assets/Scripts/MyCharacterController.cs
@@ -19,29 +19,19 @@ public class MyCharacterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxis("Vertical") <= 0.01f)
-        {
-            animator.SetBool("isWalking", false);
-            animator.SetBool("isRunning", false);
-            //Debug.Log($"isWalking = {animator.GetBool("isWalking")}, isRunning = {animator.GetBool("isRunning")}");
+        if (Input.GetAxis("Vertical") >= 0.1f) animator.SetBool("goingForward", true);
+        else animator.SetBool("goingForward", false);
 
-        }
-        else if (Input.GetButton("Fire3"))
-        {
-            animator.SetBool("isWalking", false);
-            animator.SetBool("isRunning", true);
-            //Debug.Log($"isWalking = {animator.GetBool("isWalking")}, isRunning = {animator.GetBool("isRunning")}");
-        }
-        else
-        {
-            animator.SetBool("isWalking", true);
-            animator.SetBool("isRunning", false);
-            //Debug.Log($"isWalking = {animator.GetBool("isWalking")}, isRunning = {animator.GetBool("isRunning")}");
-        }
-    
+        if (Input.GetButton("Fire3")) animator.SetBool("isRunning", true);
+        else animator.SetBool("isRunning", false);
+
+        if (Input.GetAxis("Horizontal") <= -0.1f) animator.SetBool("goingLeft", true);
+        else animator.SetBool("goingLeft", false);
+
+        if (Input.GetAxis("Horizontal") >= 0.1f) animator.SetBool("goingRight", true);
+        else animator.SetBool("goingRight", false);
+
         transform.Rotate(0.0f, Input.GetAxis("Mouse X") * rotateSpeed, 0.0f);
-
-
 
     }
 }
