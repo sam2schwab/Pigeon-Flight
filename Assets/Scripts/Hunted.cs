a0eb91f638b61d6bcef93231fdd9ba0c3072e381
Melchi031 <vae_soli@live.fr>
2020-02-01 00:03:42 -0500

    Brouillon de scripts Hunter/Hunted

---
+++ Assets/Scripts/Hunted.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MyThirdPersonCamera))]
+
+public class Hunteed : MonoBehaviour
+{
+    public float minSpeed;
+    public float normalSpeed;
+    public float maxSpeed;
+
+    public float stamina;
+    public int bpm;
+
+
+    public bool isRunning;
+    public bool isHidden;
+
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
