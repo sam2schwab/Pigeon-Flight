4cca9ceca9503cff2c98828ce37e8ab4bed2e9b0
Olivier Thivierge <account2707@hotmail.com>
2020-02-02 18:27:20 -0500

    Die and Respawn

---
+++ Assets/Scripts/DieAndRespawn.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DieAndRespawn : MonoBehaviour
+{
+    GameObject[] spawns;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawns = GameObject.FindGameObjectsWithTag("Spawn");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Cat")
+        {
+            Transform respawn = spawns[0].transform;
+            float distance = 0f;
+            foreach (var item in spawns)
+            {
+                float possibleDistance = Mathf.Abs((transform.position - item.transform.position).magnitude);
+                if (possibleDistance > distance)
+                {
+                    distance = possibleDistance;
+                    respawn = item.transform;
+                }
+            }
+            transform.position = respawn.position;
+        }
+    }
+}
--- Assets/Scripts/UIManagement.cs
+++ Assets/Scripts/UIManagement.cs
@@ -13,41 +13,57 @@ public class UIManagement : MonoBehaviour
     public GameObject plumeSpawner;
     [SerializeField] float gameTimer = 1000.0f;
     [SerializeField] int plumesToCollect;
+    [SerializeField] int maxHp = 5;
 
     float currentTimer;
     Text TimerText;
     int collectedPlumes = 0;
+    int currentHp;
     // Start is called before the first frame update
     void Start()
     {
+        currentHp = maxHp;
         plumesToCollect = Mathf.Min(plumeSpawner.GetComponent<PlumesSpawner>().plumesToSpawn, plumesToCollect);
         CatWinsGo.SetActive(false);
         PigeonsWinGo.SetActive(false);
         currentTimer = gameTimer;
         TimerText = TimerGo.GetComponent<Text>();
         UpdatePlumeText(0);
+        UpdateHpText(0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (currentTimer > 0)
+        if (collectedPlumes < plumesToCollect)
         {
-            currentTimer -= Time.deltaTime;
-            TimerText.text = (currentTimer.ToString("0000") + "s");
-        }
-        else
-        {
-            CatWinsGo.SetActive(true);
+            if (currentTimer > 0)
+            {
+                currentTimer -= Time.deltaTime;
+                TimerText.text = (currentTimer.ToString("0000") + "s");
+            }
+            else
+            {
+                CatWinsGo.SetActive(true);
+            }
         }
     }
     public void UpdatePlumeText(int delta)
     {
         collectedPlumes += delta;
         PlumeCountGo.GetComponent<Text>().text = collectedPlumes.ToString() + "/" + plumesToCollect.ToString();
-        if (collectedPlumes >= plumesToCollect)
+        if (currentTimer > 0 && collectedPlumes >= plumesToCollect && currentHp > 0)
         {
             PigeonsWinGo.SetActive(true);
+        }
+    }
+    public void UpdateHpText(int delta)
+    {
+        maxHp -= delta;
+        HpCountGo.GetComponent<Text>().text = currentHp.ToString() + "/" + maxHp.ToString();
+        if (currentHp <= 0 && collectedPlumes < plumesToCollect)
+        {
+            CatWinsGo.SetActive(true);
         }
     }
 }
