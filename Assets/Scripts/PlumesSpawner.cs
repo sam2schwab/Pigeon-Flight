d83b1d05a1a9571a8eb2749d2e7e5dc078e3ed46
Olivier Thivierge <account2707@hotmail.com>
2020-02-02 16:27:42 -0500

    Plume Spawn

---
+++ Assets/Scripts/PlumesSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlumesSpawner : MonoBehaviour
+{
+    List<Transform> spawnPoints = new List<Transform>();
+    [SerializeField] int plumesToSpawn = 10;
+    [SerializeField] GameObject plumeTemplate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Transform child in transform)
+        {
+            spawnPoints.Add(child);
+        }
+        plumesToSpawn = Mathf.Min(plumesToSpawn, spawnPoints.Count);
+
+        for (int i = 0; i < plumesToSpawn; i++)
+        {
+            int index = Random.Range(0, spawnPoints.Count);
+            Instantiate(plumeTemplate, spawnPoints[index].transform.position, Quaternion.identity);
+            spawnPoints.Remove(spawnPoints[index]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
