311d7761c7db7372d3b8e225297f4fb9bd2a9970
Olivier Thivierge <account2707@hotmail.com>
2020-02-02 15:36:17 -0500

    Portals!

---
+++ Assets/Scripts/Plume.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Plume : MonoBehaviour
+{
+    [SerializeField] float RotationSpeed = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate (0,RotationSpeed*Time.deltaTime,0); //rotates X degrees per second around z axis
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            UpdateScore();
+            Destroy(gameObject);
+        }       
+    }
+    
+    void UpdateScore()
+    {
+        // INSERT LAN STUFF HERE!
+    }
+}
