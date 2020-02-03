cf62fb5a6f993ded2a7709eed8c7cf8463d6219d
Olivier Thivierge <account2707@hotmail.com>
2020-02-02 20:53:05 -0500

    ???

--- Assets/Scripts/Plume.cs
+++ Assets/Scripts/Plume.cs
@@ -9,6 +9,7 @@ public class Plume : NetworkedBehaviour
     [SerializeField] float RotationSpeed = 10.0f;
     // Start is called before the first frame update
 
+    [SerializeField] AudioClip plumeClip;
     GameObject UIManager;
 
     void Start()
@@ -19,20 +20,21 @@ public class Plume : NetworkedBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate (0,RotationSpeed*Time.deltaTime,0); //rotates X degrees per second around z axis
+        transform.Rotate(0, RotationSpeed * Time.deltaTime, 0); //rotates X degrees per second around z axis
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
+            other.gameObject.GetComponent<AudioSource>().PlayOneShot(plumeClip);
             Debug.Log("player interacts");
-            Debug.Log("player is"+ (IsLocalPlayer ? " local" : "remote"));
+            Debug.Log("player is" + (IsLocalPlayer ? " local" : "remote"));
             UpdateScore();
             Destroy(gameObject);
-        }       
+        }
     }
-    
+
     void UpdateScore()
     {
         UIManager.GetComponent<UIManagement>().UpdatePlumeText(1);
